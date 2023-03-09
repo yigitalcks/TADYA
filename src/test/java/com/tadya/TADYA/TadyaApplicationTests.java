d08c1ec855ff2faae1b79803a13e648b03e4ca95
yigitalcks <yigitalkcs0@outlook.com>
2023-03-09 12:46:46 +0300

    .

---
+++ src/test/java/com/tadya/TADYA/TadyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadya.TADYA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
